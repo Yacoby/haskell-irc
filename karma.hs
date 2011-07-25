3c8ca98aa2c8f1aa45937a28677e94a83300fd42
Jacob Essex <jacob@jacobessex.com>
2011-07-25 22:44:17 +0100

    Fixed up some code, made things neater

--- karma.hs
+++ karma.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+--because sometimes specifing types is a pita
+{-# LANGUAGE NoMonomorphismRestriction  #-}
 
 import Network
 import System.IO
@@ -11,94 +12,143 @@ import Data.List
 
 import Text.ParserCombinators.Parsec
 
+-- -----------------------------------------------------------------------------
 -- constants
 server = "irc.imaginarynet.org.uk"
-port = 6667
+port   = 6667
+nick   = "KarmaBot"
+user   = "KarmaBot"
+name   = "Yacoby v2"
 
+-- -----------------------------------------------------------------------------
 
 data IrcStream = IrcStream { irc :: Handle } 
 
 type IrcLogger = ReaderT IrcStream IO
 
-
 main :: IO ()
 main  = do
-    h <- connect
-    hPutStrLn h "NICK TEST"
-    hPutStrLn h "USER TestUsr 0 * :YacobyIsAwesome"
-    runReaderT runBot (IrcStream h)
-    return ()
+    h <- connect server port nick user
+    runBot ircOutput h
 
-runBot :: IrcLogger ()
-runBot = forever $ do
+ircOutput :: Response -> IrcLogger ()
+ircOutput (Response _ (IntCmd "004" _ msg)) = write "JOIN #yac"
+ircOutput _                                 = return ()
+
+-- -----------------------------------------------------------------------------
+-- Lower level irc stuff
+
+runBot :: (Response -> IrcLogger ()) -> Handle -> IO ()
+runBot output handle = runReaderT (mainLoop output) (IrcStream handle)
+
+connect :: String -> Int -> String -> String -> IO Handle
+connect server port nick name = do
+    h <- connectTo server $ PortNumber $ fromIntegral port
+    hSetBuffering h NoBuffering
+    hPutStrLn h $ "NICK " ++ nick
+    hPutStrLn h $ "USER " ++ nick ++ " 0 * :" ++ name
+    return h
+
+mainLoop :: (Response -> IrcLogger () ) -> IrcLogger ()
+mainLoop outfunc = forever $ do
     h <- asks irc
-    line <- liftIO (hGetLine h)
+    line <- liftIO $ hGetLine h
     liftIO $ putStrLn line
-    msgDispatch (parseMessage line)
-    return ()
+    maybeDispatch outfunc $ parseMessage line
     where
         forever a = a >> forever a
 
-msgDispatch :: Maybe Message -> IrcLogger ()
-msgDispatch Nothing  = liftIO $ putStrLn "++PARSE FAIL++"  --return ()
-msgDispatch (Just m) = processMessage m
+maybeDispatch :: (Response -> IrcLogger() ) -> Maybe Response -> IrcLogger ()
+maybeDispatch outputf response = maybe deflt processMessage response
+    where
+        --default, general debug
+        deflt = liftIO $ putStrLn "++PARSE FAIL++"
 
-joinChan = do write "JOIN #yac"
-
-dealWithMsg :: Message -> IrcLogger ()
-dealWithMsg (Msg host "004" to msg)     = joinChan
-dealWithMsg (Msg host "PRIVMSG" to msg) = liftIO $ putStrLn ( "<< " ++ msg )
-dealWithMsg _                           = return ()
-
-processMessage :: Message -> IrcLogger ()
-processMessage (Ping   m)   = write ("PONG :" ++ m) --catch this, as never needed
-processMessage m            = dealWithMsg m
+        --we don't need to see the pong any higher up, there is no point
+        --as it just adds complexity to higher levels
+        processMessage :: Response -> IrcLogger ()
+        processMessage (Response _ (Ping  m)) = write $ "PONG :" ++ m
+        processMessage response               = outputf response
 
 write :: String -> IrcLogger ()
 write msg = do
     h <- asks irc
     liftIO $ hPutStrLn h msg
+
+    --debug
     liftIO $ putStrLn $ ">>" ++ msg
 
-connect :: IO Handle
-connect = do
-    h <- connectTo server $ PortNumber $ fromIntegral port
-    hSetBuffering h NoBuffering
-    return h
--- -----------------------------------------------
+
+-- --------------------------------
 -- Simple mesage parser, very basic
 
-data Message = Ping String
-             | Notice
-             | Msg String String String String
-             deriving (Show)
+data Response = Response { host ::  Maybe Host,
+                           message  :: Message }
+                          deriving (Show, Eq)
 
-pingMsg = do
+
+data Host = Host { hostName :: String,
+                   hostDetails :: String }
+                  deriving (Show, Eq)
+
+hostMask = do
+    char ':'
+    host <- many $ noneOf "! "
+    other <- option "" parseRest
+    return $ Host host other
+    where
+        parseRest = do 
+            str <- many (noneOf " ")
+            char ' '
+            return str
+
+-- -----------------------
+-- Basic commands/messges
+data Message = Ping String
+             | Notice { nickname :: String, text :: String }
+             | PrivateMsg  { destination :: String, contents :: String }
+             | IntCmd { intCmd :: String, destination :: String, contents :: String }
+             deriving (Show, Eq)
+
+cmdPrivateMsg = do
+    string "PRIVMSG "
+    dest <- many $ noneOf " "
+    string " :"
+    msg <- many anyChar
+    return $ PrivateMsg dest msg
+
+cmdPing = do
     string "PING :"
     msg <- many alphaNum
     return $ Ping msg
 
-noticeMsg = do
-    string "NOTICE"
-    return $ Notice
-
-housekeepingMsg = (pingMsg <|> noticeMsg)
-
-visibleMessage = do
-    char ':'
-    host <- many (noneOf " ")
+cmdInt = do
+    num <- many digit
     char ' '
-    cmd <- many alphaNum
+    dest <- many (noneOf " ")
     char ' '
-    to <- many (noneOf " ")
-    try (string " :") <|> (string " ")
     rest <- many anyChar
+    return $ IntCmd num dest rest
 
-    return $ Msg host cmd to rest
+cmdNotice = do
+    string "NOTICE "
+    nick <- many (noneOf " ")
+    char ' '
+    text <- many anyChar
+    return $ Notice nick text
 
-parseMessage :: String -> Maybe Message
-parseMessage msg = case parse (visibleMessage <|> housekeepingMsg) "" msg of
+cmd = try cmdNotice <|>
+      try cmdPing <|>
+      try cmdInt  <|>
+      cmdPrivateMsg
+
+cmdMsg = do
+    host <- optionMaybe hostMask
+    msg <- cmd
+    return $ Response host msg
+
+
+parseMessage :: String -> Maybe Response
+parseMessage msg = case parse cmdMsg "" msg of
                     Left e  -> Nothing
                     Right r -> Just r
--- -----------------------------------------------
-
